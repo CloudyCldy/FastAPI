11f4dcce83e9177335e562e3fa15f659b01fce1f
Cloudy <cloudysa139@gmail.com>
2025-04-02 22:48:57 -0600

    ...

--- main.py
+++ main.py
@@ -355,4 +355,4 @@ async def get_sensores(db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
